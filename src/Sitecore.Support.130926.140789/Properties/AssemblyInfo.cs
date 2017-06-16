40a1852536d1f376ce84b0acb16a7e1a3d9f8c2b
DK\msb <msb@sitecore.net>
2017-06-16 15:29:08 -0400

    Initail one

---
+++ src/Sitecore.Support.130926.140789/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.130926.140789")]
+[assembly: AssemblyProduct("Sitecore.Support.130926.140789")]
+[assembly: ComVisible(false)]
